31d2c3f5939c496ef7ca87b091debeb30d566a11
Anusha Ramesh Badiger <150438836+AnushaRB@users.noreply.github.com>
2023-11-16 12:35:12 +0530

    Update Myclass.java

--- src/practicepgms/Myclass.java
+++ src/practicepgms/Myclass.java
@@ -3,8 +3,11 @@ package practicepgms;
 public class Myclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!!!!!!");
+		System.out.println("Welcome to Java Programming Language");
+		System.out.println("I");
+		System.out.println("Love");
+		System.out.println("Java");
 	}
 
 }
